86327cab289e8f4ae7bdfd2b3b8e4f6ace59b012
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-02-24 19:22:51 +0200

    static methods

--- Person.cs
+++ Person.cs
@@ -8,17 +8,36 @@ namespace Classes
     {
         public string FirstName;
         public string LastName;
+        private DateTime dateOfBirth;
+        private string contactNumber;
+
+        public static int Count = 0;
+
+        public string ContactNumber
+        {
+            get { return contactNumber; }
+            set { 
+                if (value.Length < 9)
+                {
+                    Console.WriteLine("Invalid contact number length.");
+                } else
+                {
+                    contactNumber = value;
+                }
+            }
+        }
+        // public string ContactNumber { get; set; }; // auto implemented getters and setters sould only used if there is no additional logic in the code block, i.e. input validation. 
         public Person(string firstName, string lastName)
         {
             FirstName = firstName;
             LastName = lastName;
+            Count++;
         }
         public Person(DateTime dateOfBirth,string firstName, string lastName) : this(firstName, lastName)
         {
             SetDateOfBirth(dateOfBirth);
         }
 
-        private DateTime dateOfBirth;
         public void SetDateOfBirth(DateTime date)
         {
             if (date > DateTime.Now)
--- Program.cs
+++ Program.cs
@@ -9,9 +9,13 @@ namespace Classes
             Person Edward = new Person("Edward", "Snowden");
             Edward.SetDateOfBirth(new DateTime(1982, 2, 14));
             Edward.greeting();
+            Edward.ContactNumber = "74946565923";
+            Console.WriteLine($"My phone number is :{Edward.ContactNumber}.");
 
             Person Wallace = new Person(new DateTime(1952, 4, 29), "Wallace", "Breen");
             Wallace.greeting();
+
+            Console.WriteLine($"Object of Person type count: {Person.Count}");
         }
     }
 }
