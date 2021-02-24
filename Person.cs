c207e1c1d69d3bf615a656f01748fe379dfffac5
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-02-24 18:56:53 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public Person(DateTime dateOfBirth,string firstName, string lastName) : this(firstName, lastName)
+        {
+            SetDateOfBirth(dateOfBirth);
+        }
+
+        private DateTime dateOfBirth;
+        public void SetDateOfBirth(DateTime date)
+        {
+            if (date > DateTime.Now)
+            {
+                Console.WriteLine("Invalid date of birth provided.");
+            } else
+            {
+                dateOfBirth = date;
+            }
+        }
+        public DateTime GetDateOfBirth() => dateOfBirth;
+
+        public void greeting()
+        {
+            Console.WriteLine($"Hello, my name is {this.FirstName} {this.LastName}, and I was born on {this.dateOfBirth}.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Edward = new Person("Edward", "Snowden");
+            Edward.SetDateOfBirth(new DateTime(1982, 2, 14));
+            Edward.greeting();
+
+            Person Wallace = new Person(new DateTime(1952, 4, 29), "Wallace", "Breen");
+            Wallace.greeting();
+        }
+    }
+}
